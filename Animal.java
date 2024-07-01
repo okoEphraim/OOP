72cfcd768056c869c2fdc1c46d66bba8b33df4cc
okoEphraim <132933828+okoEphraim@users.noreply.github.com>
2024-07-01 10:22:38 +0100

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,13 @@
+
+
+public class Animal {
+    String name;
+    String move(){
+        return "Moves by walking";
+    }
+    boolean isALive(){
+        return true;
+    }
+
+
+}
---
+++ Physics.java
@@ -0,0 +1,10 @@
+
+
+public class Physics {
+    float calcVel(float distance, int time){
+        return distance/time;
+    }
+    float calcVel(float initialVal, int acceleration, int time){
+        return initialVal + (acceleration * time);
+    }
+}
